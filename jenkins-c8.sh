5e715e9c1c5a2adba1b36f7f7e8106190ce4977b
MichaelGit-beep <61975960+MichaelGit-beep@users.noreply.github.com>
2021-03-04 19:04:36 +0200

    Create jenkins-c8.sh

---
+++ jenkins-c8.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+################################## Add repo
+echo "Adding jenkins repo"
+wget -O /etc/yum.repos.d/jenkins.repo https://pkg.jenkins.io/redhat-stable/jenkins.repo > /dev/null 2>&1
+
+if [ $? -eq 0 ]
+then
+echo "Jenkins repo successfully added into the system"
+else
+echo "Adding Jenkins repo failed"
+fi
+
+
+################################# Add gpg-pubkey for rpm package from added repo
+echo "Importing gpg-key"
+rpm --import https://pkg.jenkins.io/redhat-stable/jenkins.io.key > /dev/null 2>&1
+
+if [ $? -eq 0 ]
+then
+echo "gpg-pubkey successfully added "
+else
+echo "Adding gpg-pubkey failed"
+fi
+
+
+################################ Install Jenkins and Java
+echo "Installing Jenkins"
+dnf -y install jenkins java-1.8.0-openjdk-devel > /dev/null 2>&1
+if [ $? -eq 0 ]
+then
+echo "Jenkins Installed"
+else
+echo "Jenkins Installation Failed"
+fi
+
+
+############################# Configure Jenkins service startup
+systemctl --now enable jenkins.service
+firewall-cmd --add-port=8080/tcp
