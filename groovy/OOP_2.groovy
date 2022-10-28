0dd7175f91389755220b4bf829f5f0fbda1b8a91
Akiles <vareikismihail@gmail.com>
2022-10-28 17:17:19 +0300

    123

--- groovy/OOP_2.groovy
+++ groovy/OOP_2.groovy
@@ -1,2 +1,6 @@
-package cars
+class Car {
+    int x,y,z 
+}
 
+a = new Car(x: 2, y:2,z:3)
+println(a.z)
---
+++ groovy/lists.groovy
@@ -0,0 +1,3 @@
+def list1 = ["value1", "value2"]
+def newI = list1.find { it == "value1" }
+println(newI)
